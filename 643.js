57ac856b8cd1f5c98e4187cb2134452c84d3c14e
Vinícius Aquino <vinicius.aquino@estudante.ufcg.edu.br>
2024-11-17 17:57:43 -0300

    Create 643.js

---
+++ 643.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findMaxAverage = function(nums, k) {
+    let sum = 0;
+
+    for (let i = 0; i < k; i++){
+        sum += nums[i];
+    }
+
+    let avg = sum/k;
+
+    for (let i = k; i < nums.length; i++){
+        sum = sum + nums[i] - nums[i-k];
+        avg = Math.max(avg, sum / k);
+    }
+    return (avg);
+};
